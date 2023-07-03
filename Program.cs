61853374342a80f5613f6c476bb86746a4626f1e
Emma <em.lebedeva85@gmail.com>
2023-07-03 11:50:03 +0300

    Create Test1Quarter

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+    
+string[] GetArray(int length) 
+{     
+    string[] array = new string[length];     
+    for (int i = 0; i < length; i++)     
+    {         
+        Console.WriteLine("Введите значения массива");
+        array[i] = Console.ReadLine();     
+    }     
+    return array; 
+}  
+   
+int GetLength() 
+{     
+    Console.WriteLine("Введите длину массива");     
+    int length = Convert.ToInt32(Console.ReadLine());     
+    return length; 
+}  
+
+
+
+int length = GetLength(); 
+string[] array = GetArray(length); 
